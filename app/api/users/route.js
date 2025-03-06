daa9fc295a931a90c1a8b50faa52a636f9340dfe
maximiloi <maximiloi@gmail.com>
2025-03-06 17:30:53 +0300

    fix: delete get users response

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -3,11 +3,6 @@ import { NextResponse } from 'next/server';
 
 const prisma = new PrismaClient();
 
-export async function GET() {
-  const users = await prisma.user.findMany();
-  return NextResponse.json(users);
-}
-
 export async function POST(req) {
   const body = await req.json();
   const user = await prisma.user.create({ data: body });
