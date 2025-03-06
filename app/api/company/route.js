704625729f162ba3ac36ace5fd2a7f4181cc1be2
maximiloi <maximiloi@gmail.com>
2025-03-06 17:30:28 +0300

    feat: add user table companyId at create company info

--- app/api/company/route.js
+++ app/api/company/route.js
@@ -1,19 +1,39 @@
 import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]/route';
 
 const prisma = new PrismaClient();
 
 export async function POST(req) {
   try {
+    const session = await getServerSession(authOptions);
+    if (!session?.user?.id) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
+    const userId = session.user.id;
     const data = await req.json();
 
-    const newGroup = await prisma.company.create({
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      select: { companyId: true },
+    });
+
+    const newCompany = await prisma.company.create({
       data: {
         ...data,
       },
     });
 
-    return NextResponse.json(newGroup, { status: 201 });
+    if (!user?.companyId) {
+      await prisma.user.update({
+        where: { id: userId },
+        data: { companyId: newCompany.id },
+      });
+    }
+
+    return NextResponse.json(newCompany, { status: 201 });
   } catch (error) {
     console.error('Ошибка создания компании:', error);
     return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
