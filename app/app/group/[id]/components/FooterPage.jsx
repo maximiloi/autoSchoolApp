b0bcbd2c38f447242df05d69778b261190ca65b6
maximiloi <maximiloi@gmail.com>
2025-03-10 15:36:24 +0300

    feat add template informationForTrafficPolice

--- app/app/group/[id]/components/FooterPage.jsx
+++ app/app/group/[id]/components/FooterPage.jsx
@@ -1,14 +1,12 @@
-import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
-import { ScrollText } from 'lucide-react';
+
+import InformationForTrafficPoliceButton from './InformationForTrafficPoliceButton';
 import ScheduleTemplateButton from './ScheduleTemplateButton';
 
 export default function FooterPage({ group, company }) {
   return (
     <div className="flex h-5 items-center space-x-4 text-sm">
-      <Button variant="secondary">
-        <ScrollText /> Путевой лист
-      </Button>
+      <InformationForTrafficPoliceButton group={group} company={company} />
       <Separator orientation="vertical" />
       <ScheduleTemplateButton group={group} company={company} />
     </div>
---
+++ templates/informationForTrafficPolice.js
@@ -0,0 +1,199 @@
+import { format } from 'date-fns';
+import { ru } from 'date-fns/locale';
+
+export default function informationForTrafficPoliceTemplate(group, company) {
+  const { companyName, actualAddress } = company;
+  const { category, startTrainingDate, endTrainingDate, students } = group;
+
+  const cleanedAddress = (address) => address.replace(/^\d{6},\s*/, '');
+  const sortedStudents = students.sort((a, b) => a.studentNumber - b.studentNumber);
+
+  return {
+    pageOrientation: 'landscape',
+    content: [
+      { text: 'Начальнику ОГИБДД ОМВД РФ', style: 'pageHeader', alignment: 'right' },
+      { text: 'по Окуловскому району', style: 'pageHeader', alignment: 'right' },
+      { text: 'майору полиции Рыжову С.М.', style: 'pageHeader', alignment: 'right' },
+      { text: 'СВЕДЕНИЯ', style: 'header' },
+      {
+        text: `о лицах, обучающих по программе подготовки водителей транспортных средств категории "${category}"`,
+        style: 'header',
+      },
+      {
+        text: `Организация: ${companyName} ${cleanedAddress(actualAddress)}`,
+        style: 'subHeader',
+        margin: [0, 2, 0, 0],
+      },
+      {
+        text: `Категория (подкатегория) ТС "${category}" Дата начала обучения: ${format(new Date(startTrainingDate), 'dd/MM/yyyy', { locale: ru })}. Дата окончания: ${format(new Date(endTrainingDate), 'dd/MM/yyyy', { locale: ru })}.`,
+        style: 'subHeader',
+        margin: [0, 0, 0, 2],
+      },
+      {
+        style: 'tableExample',
+        margin: [0, 5, 0, 0],
+        fontSize: 10,
+        table: {
+          widths: [
+            '3%',
+            '8%',
+            '8%',
+            '8%',
+            '3.5%',
+            '9%',
+            '13%',
+            '4%',
+            '8%',
+            '8%',
+            '12.5%',
+            '6%',
+            '9%',
+          ],
+          body: [
+            [
+              { rowSpan: 2, text: '№ п/п', style: 'tableHeader' },
+              { colSpan: 7, text: 'Анкетные данные', style: 'tableHeader' },
+              {},
+              {},
+              {},
+              {},
+              {},
+              {},
+              { colSpan: 5, text: 'Документ, удостоверяющий личность', style: 'tableHeader' },
+              {},
+              {},
+              {},
+              {},
+            ],
+            [
+              {},
+              { text: 'Фамилия', style: 'tableHeader' },
+              { text: 'Имя', style: 'tableHeader' },
+              { text: 'Отчество', style: 'tableHeader' },
+              { text: 'Пол', style: 'tableHeader' },
+              { text: 'Дата рождения', style: 'tableHeader' },
+              { text: 'Место рождения', style: 'tableHeader' },
+              { text: 'Гражданство', style: 'tableHeader' },
+              { text: 'Наименование документа', style: 'tableHeader' },
+              { text: 'Серия, номер', style: 'tableHeader' },
+              { text: 'Кем выдан', style: 'tableHeader' },
+              { text: 'Код подразделения', style: 'tableHeader' },
+              { text: 'Дата выдачи', style: 'tableHeader' },
+            ],
+            ...sortedStudents.map((student) => [
+              { text: student.studentNumber, alignment: 'center' },
+              { text: student.lastName },
+              { text: student.firstName },
+              { text: student.middleName || '' },
+              { text: student.gender === 'male' ? 'М' : 'Ж', alignment: 'center' },
+              {
+                text: format(new Date(student.birthDate), 'dd/MM/yyyy', { locale: ru }),
+                alignment: 'center',
+              },
+              { text: student.birthPlace },
+              { text: 'РФ', alignment: 'center' },
+              { text: student.documentType === 'passport' ? 'паспорт РФ' : 'паспорт Азербайджана' },
+              { text: `${student.documentSeries} ${student.documentNumber}` },
+              { text: student.documentIssuer },
+              { text: student.documentCode },
+              {
+                text: student.documentIssueDate
+                  ? format(new Date(student.documentIssueDate), 'dd/MM/yyyy', { locale: ru })
+                  : '',
+                alignment: 'center',
+              },
+            ]),
+          ],
+        },
+      },
+      { text: '', pageBreak: 'before' },
+      {
+        style: 'tableExample',
+        margin: [0, 5, 0, 0],
+        fontSize: 10,
+        table: {
+          widths: [
+            '3%',
+            '4%',
+            '8%',
+            '8%',
+            '8%',
+            '4%',
+            '4%',
+            '4%',
+            '6%',
+            '9%',
+            '8%',
+            '12%',
+            '6%',
+            '12%',
+          ],
+          body: [
+            [
+              { rowSpan: 2, text: '№ п/п', style: 'tableHeader' },
+              { colSpan: 7, text: 'Адрес регистрации (пребывания)', style: 'tableHeader' },
+              {},
+              {},
+              {},
+              {},
+              {},
+              {},
+              { colSpan: 5, text: 'Медицинское заключение', style: 'tableHeader' },
+              {},
+              {},
+              {},
+              {},
+              { text: 'Подпись кандидата в водители', style: 'tableHeader' },
+            ],
+            [
+              {},
+              { text: 'Cтрана', style: 'tableHeader' },
+              { text: 'Регион', style: 'tableHeader' },
+              { text: 'Населенный пункт', style: 'tableHeader' },
+              { text: 'Улица', style: 'tableHeader' },
+              { text: 'дом', style: 'tableHeader' },
+              { text: 'корпус', style: 'tableHeader' },
+              { text: 'квартира', style: 'tableHeader' },
+              { text: 'Серия номер', style: 'tableHeader' },
+              { text: 'Дата выдачи', style: 'tableHeader' },
+              { text: 'Наименование мед.организации', style: 'tableHeader' },
+              { text: 'серия и номер лицензии', style: 'tableHeader' },
+              { text: 'серия и номер вод. уд-ния', style: 'tableHeader' },
+              {
+                text: 'С обработкой персональных данных в соответствии с Фед.законом от 27.07.2006 №152-ФЗ "О персональных данных" согласен',
+                style: 'tableHeader',
+              },
+            ],
+            ...sortedStudents.map((student) => [
+              { text: student.studentNumber, alignment: 'center' },
+              { text: 'РФ', alignment: 'center' },
+              { text: student.registrationAddress },
+              { text: '' },
+              { text: '' },
+              { text: '' },
+              { text: '' },
+              { text: '' },
+              { text: `${student.medicalSeries} ${student.medicalNumber}` },
+              {
+                text: format(new Date(student.medicalIssueDate), 'dd/MM/yyyy', { locale: ru }),
+                alignment: 'center',
+              },
+              { text: student.medicalIssuer },
+              { text: `${student.licenseSeries} ${student.license}` },
+              { text: '' },
+              {
+                text: '',
+              },
+            ]),
+          ],
+        },
+      },
+    ],
+    styles: {
+      pageHeader: { fontSize: 10, bold: true },
+      header: { fontSize: 12, bold: true, alignment: 'center' },
+      subHeader: { fontSize: 10 },
+      tableHeader: { fontSize: 8, alignment: 'center' },
+    },
+  };
+}
