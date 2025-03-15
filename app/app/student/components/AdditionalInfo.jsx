3c3de19018d40973d4bbc93ee8c2c22a5b877e81
maximiloi <maximiloi@gmail.com>
2025-03-15 11:06:40 +0300

    feat: add new input

--- app/app/student/components/AdditionalInfo.jsx
+++ app/app/student/components/AdditionalInfo.jsx
@@ -4,7 +4,9 @@ export default function AdditionalInfo({ control }) {
   return (
     <>
       <h3 className="text-sm font-semibold">Дополнительная информация</h3>
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-3 gap-4">
+        <InputField name="education" label="Образование" control={control} />
+        <InputField name="placeOfWork" label="Место работы" control={control} />
         <InputField name="medicalRestriction" label="Мед. ограничение" control={control} />
         <InputField name="allowedCategories" label="Разрешенные категории ТС" control={control} />
         <InputField name="trainingCost" label="Стоимость обучения" control={control} />
