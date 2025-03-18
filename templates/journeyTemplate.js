317b284c1739851baf2ad87be7d4f9893dd21ba6
maximiloi <maximiloi@gmail.com>
2025-03-18 11:20:33 +0300

    delete unuse console

--- templates/journeyTemplate.js
+++ templates/journeyTemplate.js
@@ -2,7 +2,6 @@ import { format } from 'date-fns';
 import { ru } from 'date-fns/locale';
 
 import { SCHEDULE } from '@/lib/schedule';
-console.log('🚀 ~ SCHEDULE:', SCHEDULE);
 
 export default function journeyTemplate(group = {}, company = {}) {
   const { companyName } = company;
