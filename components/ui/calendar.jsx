07a8019dfb08c298b0edcb8e0307113d6ebad495
maximiloi <maximiloi@gmail.com>
2025-06-04 20:50:24 +0300

    fix: eslint error ui component

--- components/ui/calendar.jsx
+++ components/ui/calendar.jsx
@@ -1,10 +1,9 @@
 'use client';
-import * as React from 'react';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 import { DayPicker } from 'react-day-picker';
 
-import { cn } from '@/lib/utils';
 import { buttonVariants } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
 
 function Calendar({ className, classNames, showOutsideDays = true, ...props }) {
   return (
