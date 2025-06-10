e5ac2030f34b070c6d0fc2516c7ead9ef883b016
maximiloi <maximiloi@gmail.com>
2025-06-10 12:38:20 +0300

    refactor: dropdown buttons bread in individual component

---
+++ app/app/group/[id]/components/ButtonsGroupReminder.jsx
@@ -0,0 +1,38 @@
+import { MessageSquareReply } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import WhatsAppButton from './WhatsAppButton';
+import WhatsAppButton1 from './WhatsAppButton1';
+
+export default function ButtonsGroupReminder({ student }) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <MessageSquareReply />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>Отправка сообщений в WhatsApp</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <WhatsAppButton student={student} />
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <WhatsAppButton1 student={student} />
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
