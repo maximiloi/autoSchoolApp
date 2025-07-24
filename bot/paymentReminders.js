5ba0f83d4752a15e6c4f5dff0b0f626b055fea73
maximiloi <maximiloi@gmail.com>
2025-07-24 09:27:32 +0300

    chore: change data

--- bot/paymentReminders.js
+++ bot/paymentReminders.js
@@ -5,7 +5,7 @@ import { sendTelegramMessage } from './sendTelegramMessage.js';
 const prisma = new PrismaClient();
 const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN);
 const dateFirstReminder = 8;
-const dateSecondReminder = 23;
+const dateSecondReminder = 24;
 
 const adminChatIds =
   process.env.TELEGRAM_ADMIN_CHAT_ID?.split(',')
